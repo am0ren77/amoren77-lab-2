037bd1ba8ab9a0e09aab5b414f321d08516b6d5d
ashleymoreno <amoren77@calpoly.edu>
2024-10-11 21:51:39 -0700

    Airlines checked

---
+++ AIRLINES-setup.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS airlines;
+DROP TABLE IF EXISTS airports;
+DROP TABLE IF EXISTS airlines;
+
+CREATE TABLE airlines (
+  Id integer PRIMARY KEY,
+  Airline varchar(100),
+  Abbreviation varchar(50),
+  Country varchar(50),
+  UNIQUE(Abbreviation)
+);
+
+CREATE TABLE Airports (
+    AirportCode VARCHAR(10) PRIMARY KEY,
+    City VARCHAR(255),
+    AirportName VARCHAR(255),
+    Country VARCHAR(255),
+    CountryAbbrev VARCHAR(10),
+    UNIQUE(AirportCode)
+);
+
+CREATE TABLE flights (
+    Airline INT,
+    FlightNo INT,
+    SourceAirport VARCHAR(10) NOT NULL,
+    DestAirport VARCHAR(10) NOT NULL,
+    PRIMARY KEY (Airline, FlightNo),
+    CONSTRAINT fk_airline FOREIGN KEY (Airline) REFERENCES airlines(Id),
+    CONSTRAINT fk_source_airport FOREIGN KEY (SourceAirport) REFERENCES airports(AirportCode),
+    CONSTRAINT fk_dest_airport FOREIGN KEY (DestAirport) REFERENCES airports(AirportCode)
+);
